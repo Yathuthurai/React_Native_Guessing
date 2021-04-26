449580ee3b8c74eb453f08edc445103395745ae2
Yathurshan <tyathurshan@gmail.com>
2021-04-26 09:55:09 +0530

    Added GameScreen

---
+++ screens/GameScreen.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, Button} from 'react-native';
+
+import NumberContainer from '../components/NumberContainer';
+import Colors from '../constants/colors';
+import Card from '../components/Card';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomNumber = Math.random() * (max - min) + min;
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
+
+const GameScreen = props => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice),
+  );
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button title="LOWER" onPress={() => {}} />
+        </View>
+        <View style={styles.button}>
+          <Button title="GREATER" onPress={() => {}} />
+        </View>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%',
+  },
+  button: {
+    width: '30%',
+  },
+});
+
+export default GameScreen;
